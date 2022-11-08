9221c2030d0c80b1b9a7d2d4d04fa63f6f0bbb4c
@moritz2018.mueller <moritz2018.mueller@outlook.de>
2022-11-08 16:07:37 +0100

    Add initial boilerplate code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='iterative-ransac'
+    version='1.0',
+    author='Moritz Mueller',
+    description='Data Analysis Pipeline for Multi-Modal Emotion Recognition',
+    url='https://github.com/mo12896/emotion-recognition.git',
+    keywords='multi-modal-emotion-recognition, entanglement',
+    python_requires='>=3.8, <4'
+)
