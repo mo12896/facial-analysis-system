784106c572ee7c60acbd2643a1e4af14bf7526fe
@moritz2018.mueller <moritz2018.mueller@outlook.de>
2022-11-25 17:08:40 +0100

    Refactoring Face Detector and their base class

--- src/datahandler/dataprocessor/face_detector.py
+++ src/datahandler/dataprocessor/face_detector.py
@@ -1,90 +1,44 @@
-from typing import Protocol, Tuple
+from abc import ABC, abstractmethod
+from typing import Tuple
 
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 from retinaface import RetinaFace
 
 OPENCV_MODEL = "/home/moritz/anaconda3/envs/emotion/lib/python3.10/site-packages/cv2/data/haarcascade_frontalface_default.xml"
 
 
-class FaceDetectorFactory:
-    @staticmethod
-    def create_face_detector(detector: str) -> "FaceDetector":
-        if detector == "retinaface":
-            return RetinaFaceDetector()
-        elif detector == "opencv":
-            face_detector = cv2.CascadeClassifier(OPENCV_MODEL)
-            return OpenCVFaceDetector(face_detector)
-        else:
-            raise ValueError("Invalid face detector")
-
-
-class FaceDetector(Protocol):
+class FaceDetector(ABC):
+    @abstractmethod
     def __init__(self, face_detector):
-        ...
+        """Base constructor for all face detectors.
 
-    def detect_faces(self, frame: np.ndarray) -> Tuple[list, np.ndarray]:
-        ...
-
-    def display_faces(self, frame: np.ndarray) -> np.ndarray:
-        ...
-
-
-class OpenCVFaceDetector:
-    """Face detector using OpenCV's Haar Cascade Classifier."""
-
-    def __init__(self, face_detector: cv2.CascadeClassifier):
+        Args:
+            face_detector: Face detector object.
+        """
         self.face_detector = face_detector
         self.bboxes = []
 
+    @abstractmethod
     def detect_faces(self, frame: np.ndarray) -> Tuple[list, np.ndarray]:
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        self.bboxes = self.face_detector.detectMultiScale(gray_frame)
-        if self.bboxes.any():
-            face_crops = [frame[y : y + h, x : x + w] for (x, y, w, h) in self.bboxes]
-            return (
-                face_crops,
-                self.bboxes,
-            )
-        else:
-            raise ValueError("No faces detected")
+        """Abstract method to detect faces in a given frame.
+
+        Args:
+            frame (np.ndarray): Current frame
+
+        Returns:
+            Tuple[list, np.ndarray]: Tuple of face crops and bounding boxes
+        """
 
     def display_faces(self, frame: np.ndarray) -> np.ndarray:
-        frame_cpy = np.copy(frame)
-        for bbox in self.bboxes:
-            cv2.rectangle(
-                frame_cpy,
-                (bbox[0], bbox[1]),
-                (bbox[0] + bbox[2], bbox[1] + bbox[3]),
-                (0, 0, 255),
-                thickness=1,
-            )
-        cv2.imshow("Detected Faces", frame_cpy)
-        return frame_cpy
+        """Displays the detected faces in a given frame.
 
+        Args:
+            frame (np.ndarray): Current frame
 
-class RetinaFaceDetector:
-    """Face detector using RetinaFace."""
-
-    def __init__(self, face_detector=RetinaFace):
-        self.face_detector = face_detector
-        self.bboxes = []
-
-    def detect_faces(self, frame: np.ndarray) -> Tuple[list, np.ndarray]:
-        faces = self.face_detector.detect_faces(frame)
-        self.bboxes = np.array([faces[face]["facial_area"] for face in faces])
-
-        if self.bboxes.any():
-            face_crops = [frame[y : y + h, x : x + w] for (x, y, w, h) in self.bboxes]
-            return (
-                face_crops,
-                self.bboxes,
-            )
-        else:
-            raise ValueError("No faces detected")
-
-    def display_faces(self, frame: np.ndarray) -> np.ndarray:
+        Returns:
+            np.ndarray: Frame with detected faces
+        """
         frame_cpy = np.copy(frame)
         for bbox in self.bboxes:
             cv2.rectangle(
@@ -92,8 +46,73 @@ class RetinaFaceDetector:
                 (bbox[0], bbox[1]),
                 (bbox[2], bbox[3]),
                 (0, 0, 255),
-                5,
+                thickness=5,
             )
         cv2.imshow("Detected Faces", frame_cpy)
-        print(self.bboxes)
         return frame_cpy
+
+
+class FaceDetectorFactory:
+    """Factory class to create face detector objects."""
+
+    @staticmethod
+    def create_face_detector(detector: str) -> FaceDetector:
+        """Creates a face detector object.
+
+        Args:
+            detector (str): Name of the face detector
+
+        Raises:
+            ValueError: If the given detector is not supported!
+
+        Returns:
+            FaceDetector: Face detector object
+        """
+        if detector == "retinaface":
+            return RetinaFaceDetector()
+        elif detector == "opencv":
+            face_detector = cv2.CascadeClassifier(OPENCV_MODEL)
+            return OpenCVFaceDetector(face_detector)
+        else:
+            raise ValueError("The chosen face detector is not supported!")
+
+
+class OpenCVFaceDetector(FaceDetector):
+    """Face detector using OpenCV's Haar Cascade Classifier."""
+
+    def __init__(self, face_detector: cv2.CascadeClassifier):
+        super().__init__(face_detector)
+
+    def detect_faces(self, frame: np.ndarray) -> Tuple[list, np.ndarray]:
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        bboxes = self.face_detector.detectMultiScale(gray_frame)
+        self.bboxes = np.array([(x, y, x + w, y + h) for (x, y, w, h) in bboxes])
+
+        if self.bboxes.any():
+            face_crops = [frame[y:h, x:w] for (x, y, w, h) in self.bboxes]
+            return (
+                face_crops,
+                self.bboxes,
+            )
+        else:
+            raise ValueError("No faces detected")
+
+
+class RetinaFaceDetector(FaceDetector):
+    """Face detector using RetinaFace."""
+
+    def __init__(self, face_detector=RetinaFace):
+        super().__init__(face_detector)
+
+    def detect_faces(self, frame: np.ndarray) -> Tuple[list, np.ndarray]:
+        faces = self.face_detector.detect_faces(frame)
+        self.bboxes = np.array([faces[face]["facial_area"] for face in faces])
+
+        if self.bboxes.any():
+            face_crops = [frame[y:h, x:w] for (x, y, w, h) in self.bboxes]
+            return (
+                face_crops,
+                self.bboxes,
+            )
+        else:
+            raise ValueError("No faces detected")
