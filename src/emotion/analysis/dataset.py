6d2022664fb1c94952cc9ae25e488f62a5b29787
@moritz2018.mueller <moritz2018.mueller@outlook.de>
2023-03-16 17:31:17 +0100

    Add dataset generator

---
+++ src/emotion/analysis/dataset.py
@@ -0,0 +1,84 @@
+# import os
+# import sys
+from pathlib import Path
+
+import pandas as pd
+
+# grandparent_folder = os.path.abspath(
+#     os.path.join(
+#         os.path.dirname(os.path.abspath(__file__)),
+#         os.pardir,
+#         os.pardir,
+#         os.pardir,
+#     )
+# )
+# sys.path.append(grandparent_folder)
+
+from src.emotion.utils.constants import IDENTITY_DIR
+
+
+def concatenate_csv_files(folder_path: Path):
+
+    files = [file_path for file_path in folder_path.glob("*.csv")]
+    df_concat = pd.DataFrame()
+
+    # Loop over each file
+    for i in range(len(files) - 1):
+        # Read the CSV file into a dataframe
+        if i == 0:
+            df_concat = pd.read_csv(files[i])
+
+        df = pd.read_csv(files[i + 1])
+
+        # Find the last frame number in the concatenated dataframe so far
+        last_frame = df_concat["Frame"].max()
+
+        # Add 5 to last_frame to get the starting frame number for the current dataframe
+        df["Frame"] = df["Frame"] + last_frame + 5
+
+        # Concatenate the current dataframe with the concatenated dataframe
+        df_concat = pd.concat([df_concat, df], ignore_index=True)
+
+    # Write the concatenated dataframe to a CSV file
+    df_concat.to_csv(
+        folder_path
+        / ("{}_{}".format(folder_path.parts[-2], folder_path.parts[-1]) + ".csv"),
+        index=False,
+    )
+
+
+if __name__ == "__main__":
+    # List of input CSV files
+    folder_path = IDENTITY_DIR / "team_20" / "2023-01-10"
+
+    concatenate_csv_files(folder_path)
+
+    teams = [
+        "team_01",
+        "team_02",
+        "team_03",
+        "team_04",
+        "team_05",
+        "team_06",
+        "team_07",
+        "team_08",
+        "team_09",
+        "team_10",
+        "team_11",
+        "team_12",
+        "team_13",
+        "team_15",
+        "team_16",
+        "team_17",
+        "team_18",
+        "team_19",
+        "team_20",
+        "team_22",
+    ]
+
+    days = ["2023-01-10", "2023-01-12", "2023-01-13"]
+
+    for team in teams:
+        for day in days:
+            folder_path = IDENTITY_DIR / team / day
+            concatenate_csv_files(folder_path)
